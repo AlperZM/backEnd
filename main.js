00cf4d0b2f76135d986eb0767e5e01171296bedf
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-01-26 10:35:32 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
-var http = require('http');
+let http = require("http"); // imports http module for server actions
+let fs = require("fs"); // imports fileSystem module for files on server
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080 
+fs.appendFile("input.html",'<input type="text" id="inp" placeholder="Input area"/>', (err)=>{ //add or update file
+ if(err){throw err;} else {console.log("savior");}
+});
+
+fs.open("gallery.html", "w", (err,file)=>{ // open file if flag "w" exsists write on the file
+ if(err){throw err;} else {console.log("savior");}
+});
+
+fs.writeFile("h3.html", "<h3>This is h3</h3>", (err)=>{ // write on the file
+   if(err){throw err;} else {console.log("savior");}
+});
+
+fs.appendFile("input.html", '<input type="color" id="col" placeholder="color picker" />',(err) =>{ //add or update file
+   if(err){throw err;} else {console.log("update");}
+});
+
+fs.unlink("h3.html", (err)=>{ // delete file
+   if(err){throw err;} else {console.log("deleted");}
+});
+
+fs.rename("input.html", "colorPicker.html", (err)=>{ /rename the file
+   if(err){throw err;} else {console.log("reneamed");}
+});
+
+http.createServer((req, res)=>{ // create server
+  fs.readFile("demo1.html",(err,data)=>{ // read the file
+    res.writeHead(200, {"content-type":"text/html"}); // declare type of file (html)
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
+
