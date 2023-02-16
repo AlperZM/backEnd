11708add45493e2c85e5809167c78da83ec8f895
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-02-16 15:14:59 +0300

    Update main.js

--- express.js/main.js
+++ express.js/main.js
@@ -1,1 +1,19 @@
-//express
+//express basic server starts...
+const express = require("express");
+const app = express();
+
+
+console.log(`Express.js initials... 
+------`);
+app.get("/",(req,res)=>{
+  res.send("Hello Node");
+});
+const server = app.listen(8081, ()=>{
+  const host = server.address().adress;
+  const port = server.address().port;
+  console.log("Express server works....");
+});
+
+
+console.log(`---------
+Express.js ends...`);
