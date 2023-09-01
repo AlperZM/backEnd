4d85add60f7980022f2e8da270fd19a60f67c7ab
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-09-01 13:11:28 +0300

    Update basics.js

--- node.js/basics.js
+++ node.js/basics.js
@@ -4,3 +4,13 @@ http.createServer(function (req, res) { //creates server app and handles request
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
 }).listen(8080); // .listen must be added
+
+// this is same but use module
+const http = require("http");
+let myTime = require("./demoModule.js"); // import and hadles module
+http.createServer((req,res)=>{
+  res.writeHead(200,{"content-type": "text/html"});
+  // access module and inside function "myTime.myDateTime()"
+  res.write("The date and time are currently: " + myTime.myDateTime());
+  res.end();
+}).listen(8080);
